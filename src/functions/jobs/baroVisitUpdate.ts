a52848f36995f39162d983d6ff011a4e4f032c32
nicholas.oreilly-cross <nicholas.oreilly-cross@roche.com>
2026-02-06 14:00:07 -0500

    fix: update schedule for weekly Baro update to run at 9:01 AM EST and notifications to fire at 9:05 est

--- src/functions/jobs/baroVisitUpdate.ts
+++ src/functions/jobs/baroVisitUpdate.ts
@@ -12,7 +12,8 @@ export async function baroVisitUpdate(myTimer: Timer, context: InvocationContext
     }
 }
 
+// Run every Friday at 9:01 AM EST (14:01 UTC)
 app.timer("baroVisitUpdate", {
-    schedule: "0 55 13 * * Fri",
+    schedule: "0 1 14 * * Fri",
     handler: baroVisitUpdate
 });
