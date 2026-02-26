db17929ada75b17287c4a8acfdcf5dffdc9369ba
nicholas.oreilly-cross <nicholas.oreilly-cross@roche.com>
2026-02-25 21:24:50 -0500

    refactor: simplify getAllItemsJob and getCurrentJob by removing mod image resolution

--- src/jobs/getAllItems.job.ts
+++ src/jobs/getAllItems.job.ts
@@ -1,7 +1,6 @@
 import { fetchAllItems } from "../services/itemService";
-import { resolveModImageSentinels } from "../services/tempModImageService";
 
 export async function getAllItemsJob() {
     const items = await fetchAllItems();
-    return resolveModImageSentinels(items);
+    return items;
 }
--- src/jobs/getCurrent.job.ts
+++ src/jobs/getCurrent.job.ts
@@ -1,10 +1,5 @@
 import { fetchCurrent } from "../services/currentService";
-import { resolveModImageSentinels } from "../services/tempModImageService";
 
 export async function getCurrentJob() {
-    const current = await fetchCurrent();
-    if (current.isActive && current.items.length > 0) {
-        current.items = await resolveModImageSentinels(current.items);
-    }
-    return current;
+    return fetchCurrent();
 }
