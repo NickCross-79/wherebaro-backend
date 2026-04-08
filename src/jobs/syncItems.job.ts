7b85be004c012c9b33106f5957c9d59bdddbe3be
nicholas.oreilly-cross <ncross7799@gmail.com>
2026-04-07 20:05:17 -0400

    feat: enhance syncItemsJob to include backfillItemData after syncing items

--- src/jobs/syncItems.job.ts
+++ src/jobs/syncItems.job.ts
@@ -1,10 +1,14 @@
 import { scrapeAndPrepareBaroItems } from "../services/wikiIngestService";
 import { syncItems } from "../services/syncService";
+import { backfillItemData } from "../services/itemService";
 
 export async function syncItemsJob() {
     console.log("Starting wiki sync process...");
 
     const baroItems = await scrapeAndPrepareBaroItems();
 
-    return syncItems(baroItems);
+    const syncResult = await syncItems(baroItems);
+    await backfillItemData();
+
+    return syncResult;
 }
